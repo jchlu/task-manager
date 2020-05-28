e499acf982fe069ccfc0bf4fc8db315ed78e5290
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-05-28 15:31:01 -0400

    findOne by ObjectId

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,4 @@
-const mongodb = require('mongodb')
-
-const MongoClient = mongodb.MongoClient
+const { ObjectId, MongoClient } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -9,7 +7,7 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
   if (error) { return console.error('Could not connect to database') }
   console.log('Connected successfully')
   const db = client.db(databaseName)
-  db.collection('users').findOne({ name: 'Wasif' }, (error, user) => {
+  db.collection('users').findOne({ _id: ObjectId('5ecff2b6cdb88170ea7e6f8f') }, (error, user) => {
     if (error) { return console.error(error) }
     console.log(user)
   })
