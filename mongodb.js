94d4c09a0f5eae790b0523a95cfc78fdf3aa981f
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-05-28 15:28:00 -0400

    dindOne user and log it out

--- mongodb.js
+++ mongodb.js
@@ -9,35 +9,8 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
   if (error) { return console.error('Could not connect to database') }
   console.log('Connected successfully')
   const db = client.db(databaseName)
-  /*  db.collection('users').insertOne({
-     name: 'Johnny',
-     age: 49
-   }, (error, result) => {
-     if (error) { return console.error('Couldn\'t insert the user') }
-     console.table(result.ops)
-   }) */
-  /* db.collection('users').insertMany([{
-    name: 'Wasif',
-    age: 28
-  }, {
-    name: 'Zuby',
-    age: 49
-  }], (error, result) => {
-    if (error) { return console.error('Couldn\'t insert documents') }
-    console.table(result.ops)
-  }) */
-  /*   db.collection('tasks').insertMany([{
-      name: 'Sweep balcony floor',
-      completed: true
-    }, {
-      name: 'Make linner',
-      completed: true
-    }, {
-      name: 'Make lunch',
-      completed: false
-    }], (error, result) => {
-      if (error) { return console.error('Couldn\'t insert documents') }
-      console.table(result.ops)
-    }) */
-  // id: 5ecff3c62c635c73bbc3731e
+  db.collection('users').findOne({ name: 'Wasif' }, (error, user) => {
+    if (error) { return console.error(error) }
+    console.log(user)
+  })
 })
