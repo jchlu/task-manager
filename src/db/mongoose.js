bcd27c934a24741d40c4a8746517ffee31ab80f0
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-05-28 20:07:57 -0400

    Clean up

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const Task = require('../models/task')
-const User = require('../models/user')
+// const Task = require('../models/task')
+// const User = require('../models/user')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager-api'
@@ -10,23 +10,3 @@ mongoose.connect(`${connectionURL}/${databaseName}`, {
   useNewUrlParser: true,
   useCreateIndex: true
 })
-
-const user = new User({
-  name: 'Snorker',
-  age: 26,
-  email: 'snorker@sausages.com      ',
-  password: 'My n3w Pa55w0rd! dot com'
-})
-
-/* user.save().then(_ => {
-  console.log(user)
-}).catch(e => { console.error(e.message) })
- */
-const task = new Task({
-  description: '     Grab a beer!   '
-})
-
-/* task.save().then(_ => {
-  console.log(task)
-}).catch(e => { console.error(e.message) })
- */
