ce716d3b423ca69b44aa706ef788cfe5438fa2e3
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-06-01 18:39:43 -0400

    Implement sorting of tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -21,10 +21,16 @@ router.post('/tasks', async (req, res) => {
 
 // READ all Tasks
 // GET /tasks?completed=[true|false]&limit=[int]&skip=[int]
+// GET /tasks?sortBy=[createdAt|updatedAt|completed]_[asc|desc]
 router.get('/tasks', async (req, res) => {
   const match = {}
+  const sort = {}
   if (req.query.completed) {
     match.completed = req.query.completed === 'true'
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split('_')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
   }
   try {
     await req.taskManagerUser.populate({
@@ -32,7 +38,8 @@ router.get('/tasks', async (req, res) => {
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort
       }
     }).execPopulate()
     res.json(req.taskManagerUser.tasks)
