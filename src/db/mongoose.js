e165c30a1d3888f93998e0247c4f969b5b8109e7
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2020-06-07 07:17:27 -0400

    Wrap mongoose connect n a try catch

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose')
 const connectionURL = process.env.MONGODB_CONNECTION_URL
 const databaseName = process.env.MONGODB_DATABASE
 
-mongoose.connect(`${connectionURL}/${databaseName}`, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-})
+const connect = async () => {
+  try {
+    await mongoose.connect(`${connectionURL}/${databaseName}`, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    })
+  } catch (e) {
+    console.error(e.message)
+  }
+}
+
+connect()
